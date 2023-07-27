73accfd889c6310985917bf4fb9a15dc73c06ecc
shotadft <98450322+shotadft0780@users.noreply.github.com>
2023-07-27 12:30:47 +0900

    Create app_prc.js

---
+++ script/app_prc.js
@@ -0,0 +1,34 @@
+let use_language = window.navigator.language;
+var language_money = 'undefined';
+
+if (use_language === 'ja-JP' || use_language === 'ja')
+{
+	language_money = '\xA5';
+}
+else if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO')
+{
+	language_money = '元';
+}
+else
+{
+	language_money = '\x24';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    function Price(Divided, Price_n){
+    this.Price_n = Price_n;
+    this.Divided = Divided;
+    const replyDivided = document.getElementById(this.Divided);
+    
+    var paragraph = document.createElement('p');
+    if(use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO')
+    {
+	paragraph.innerText = Price_n + language_money;
+    }
+    else
+    {
+	paragraph.innerText = language_money + Price_n;
+    }
+    replyDivided.appendChild(paragraph);
+}
+});
