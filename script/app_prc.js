1d2b3f169ad16329b3e355ffd9d325f14860b877
shotadft <98450322+shotadft0780@users.noreply.github.com>
2023-07-31 00:23:01 +0900

    Update app_prc.js

--- script/app_prc.js
+++ script/app_prc.js
@@ -2,40 +2,38 @@ let use_language = window.navigator.language;
 var language_money = 'undefined';
 var freePrice_text = 'Free';
 let setFreePriceText = false;
+
 if (use_language === 'ja-JP' || use_language === 'ja') {
-		language_money = '\xA5';
-		freePrice_text = '無料';
-} else if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' ||
-		use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO') {
-		language_money = '元';
-		freePrice_text = '免费';
+	language_money = '\xA5';
+	freePrice_text = '無料';
+} else if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO') {
+	language_money = '元';
+	freePrice_text = '免费';
 } else {
-		language_money = '\x24';
-		freePrice_text = 'Free';
+	language_money = '\x24';
+	freePrice_text = 'Free';
 }
+
 export function Price(Divided, Price_n) {
-		document.addEventListener("DOMContentLoaded", function () {
-				this.Price_n = Price_n;
-				this.Divided = Divided;
-				const replyDivided = document.getElementById(this.Divided);
-				var paragraph = document.createElement('p');
-				if (this.Price_n === 0) {
-						if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language ===
-								'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' ||
-								use_language === 'zh-MO') {
-								paragraph.innerText = Price_n + '(' + freePrice_text + ')' + language_money;
-						} else {
-								paragraph.innerText = language_money + Price_n + '(' + freePrice_text + ')';
-						}
-				} else {
-						if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language ===
-								'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' ||
-								use_language === 'zh-MO') {
-								paragraph.innerText = Price_n + language_money;
-						} else {
-								paragraph.innerText = language_money + Price_n;
-						}
-				}
-				replyDivided.appendChild(paragraph);
-		});
+	document.addEventListener("DOMContentLoaded", function() {
+		this.Price_n = Price_n;
+		this.Divided = Divided;
+		const replyDivided = document.getElementById(this.Divided);
+
+		var paragraph = document.createElement('p');
+		if (this.Price_n === 0) {
+			if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO') {
+				paragraph.innerText = Price_n + '(' + freePrice_text + ')' + language_money;
+			} else {
+				paragraph.innerText = language_money + Price_n + '(' + freePrice_text + ')';
+			}
+		} else {
+			if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO') {
+				paragraph.innerText = Price_n + language_money;
+			} else {
+				paragraph.innerText = language_money + Price_n;
+			}
+		}
+		replyDivided.appendChild(paragraph);
+	});
 }
